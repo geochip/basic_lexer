18b783b4d5b2b4a7c9600be9b2d085c1051d35ce
Alexander Stepchenko <x.geochip.x@yandex.ru>
2024-12-10 04:17:44 +0300

    use guessing game as an example

--- main.py
+++ main.py
@@ -59,10 +59,15 @@ class Lexer:
 
 
 code = """
-10 LET x = 5
-20 IF x > 10 THEN GOTO 30
-30 PRINT "Hello, World!"
-40 END
+10 LET secret_number = 42
+20 LET guess = 0
+30 PRINT "Guess a number between 1 and 100!"
+40 LET guess = INPUT
+50 IF guess == secret_number THEN PRINT "You win!"
+60 ELSE IF guess < secret_number THEN PRINT "Too low!"
+70 ELSE PRINT "Too high!"
+80 GOTO 40
+90 END
 """
 lexer = Lexer(code)
 tokens = lexer.lex()
